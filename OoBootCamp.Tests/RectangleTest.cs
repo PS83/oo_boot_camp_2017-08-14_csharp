0e8982c871fbb84ef3491ab0e7e87cb940badabe
Fred George <fredgeorge@acm.org>
2017-08-13 11:04:23 +0100

    Fred: Rectangle with Area()

---
+++ OoBootCamp.Tests/RectangleTest.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using NUnit.Framework;
+
+namespace OoBootCamp.Tests
+{
+    // Ensures Rectangle works correctlyS
+    [TestFixture]
+    public class RectangleTest
+    {
+        [Test]
+        public void Area()
+        {
+            Assert.AreEqual(24.0, new Rectangle(4.0, 6.0).Area());
+        }
+    }
+}
---
+++ OoBootCamp/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+ 
+namespace OoBootCamp
+{
+    // Understands a 4-sided polygon with sides at right angles
+    public class Rectangle
+    {
+        private readonly double _width;
+        private readonly double _height;
+
+        public Rectangle(double width, double height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public double Area()
+        {
+            return _height * _width;
+        }
+    }
+}
