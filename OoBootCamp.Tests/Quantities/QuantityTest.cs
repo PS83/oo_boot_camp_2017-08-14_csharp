f6eb0560697883232fb265c6b40f13811f4309eb
Fred George <fredgeorge@acm.org>
2017-08-15 12:04:20 +0100

    Fred: Quantity of same units

---
+++ OoBootCamp.Tests/Quantities/QuantityTest.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OoBootCamp.Quantities;
+using static OoBootCamp.Quantities.Quantity;
+
+namespace OoBootCamp.Tests.Quantities
+{
+    // Ensures Quantity operates correctly
+    [TestFixture]
+    public class QuantityTest
+    {
+        [Test]
+        public void EqualityOfLikeUnits()
+        {
+            Assert.AreEqual(new Quantity(4, Tablespoon), new Quantity(4, Tablespoon));
+            Assert.AreNotEqual(new Quantity(4, Tablespoon), new Quantity(6, Tablespoon));
+        }
+
+        [Test]
+        public void EqualityOfDifferentUnits()
+        {
+            Assert.AreNotEqual(new Quantity(4, Tablespoon), new Quantity(4, Teaspoon));
+        }
+    }
+}
---
+++ OoBootCamp/Quantities/Quantity.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using System;
+
+namespace OoBootCamp.Quantities
+{
+    public class Quantity : IEquatable<Quantity>
+    {
+        public static readonly object Teaspoon = new object();
+        public static readonly object Tablespoon = new object();
+        public static readonly object Ounce = new object();
+
+        private readonly double _amount;
+        private readonly object _unit;
+
+        public Quantity(double amount, object unit)
+        {
+            _amount = amount;
+            _unit = unit;
+        }
+
+        public bool Equals(Quantity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return this._amount.Equals(other._amount) && Equals(this._unit, other._unit);
+        }
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other.GetType() == this.GetType() && Equals((Quantity) other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_amount.GetHashCode() * 397) ^ (_unit != null ? _unit.GetHashCode() : 0);
+            }
+        }
+    }
+}
